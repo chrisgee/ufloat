780654ce6d06ea98c8c92f30009d6a5afbc25479
Christoph Gohle <ctg@mpq.mpg.de>
2015-01-28 13:51:30 +0100

    switch from distutils to setuptools and cleanup of setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,27 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from setuptools import setup, Extension
+#from Cython.Distutils import build_ext
 from numpy import get_include
 
 packages = ['ufloat']
-ext_modules = [#Extension("cyunits", ["cyunits.pyx"]),
-#               Extension("funit",["funit.pyx"]),
-               Extension("ufloat.ufloat",["ufloat/ufloat.pyx"])]
+ext_modules = [Extension("ufloat.ufloat",["ufloat/ufloat.pyx"])]
 requires = ['numpy']
 # You can add directives for each extension too # by attaching the ‘pyrex_directives‘
 for e in ext_modules:
 	e.pyrex_directives = {"boundscheck": False}
 
 setup(name="ufloat",
-      version="0.0.1",
+      version="0.1.0",
       description="fast physical units for scalars and numpy arrays",
       author = "Christoph Gohle, MPI für Quantenoptik",
       author_email = "christoph.gohle@mpq.mpg.de",
       ext_modules=ext_modules,
       packages = packages,
       include_dirs = [get_include()],
-      cmdclass = {"build_ext": build_ext},
-      requires = requires
+      requires = requires,
+      test_suite = 'nose.collector'
       )
 
 if __name__ == "__main__":
